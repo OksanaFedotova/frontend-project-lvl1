77053a7650c35be30b4b1859e34c8aef51e441dd
Oksana Fedotova <oxana-fedotova@yandex.ru>
2020-12-26 21:09:16 +0300

    src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export var userName;
+export let userName;
 export const greeting = () => {
-    console.log("Welcome to the Brain Games!");
-    userName = readlineSync.question('May I have your name? ');
-    console.log('Hello ' + userName + '!');
-}
+  console.log('Welcome to the Brain Games!');
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+};
