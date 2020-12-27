a0d015531a39b93a815101100afa9a0e0c81e9c8
Oksana Fedotova <oxana-fedotova@yandex.ru>
2020-12-27 18:55:00 +0300

    src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,8 +3,8 @@ import { getRandomNumber, getDivisor } from '../generalFunctions.js';
 export const description = 'Find the greatest common divisor of given numbers.';
 
 export const gameGcd = () => {
-  const randomNumberOne = getRandomNumber(0, 101);
-  const randomNumberTwo = getRandomNumber(0, 101);
+  const randomNumberOne = getRandomNumber(1, 101);
+  const randomNumberTwo = getRandomNumber(1, 101);
   const question = `${randomNumberOne} ${randomNumberTwo}`;
   const firstNumberDivisors = getDivisor(randomNumberOne);
   const secondNumberDivisors = getDivisor(randomNumberTwo);
