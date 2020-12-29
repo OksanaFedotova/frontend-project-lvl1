d6239177ad7109d1a0f860de9e7e0a9e65cea7ea
Oksana Fedotova <oxana-fedotova@yandex.ru>
2020-12-29 11:08:22 +0300

    src/games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,14 @@
+import { getRandomNumber } from '../generalFunctions.js';
+
+export const description = 'What number is missing in the progression?';
+
+export const gameProgression = () => {
+  const firstElement = getRandomNumber(0, 50);
+  const length = getRandomNumber(5, 11);
+  const difference = getRandomNumber(1, 6);
+  const arr = new Array(length).fill(0);
+  const question = arr.map((element, index) => element = firstElement + index * difference);
+  const hiddenElIndex = getRandomNumber(0, length);
+  const correctAnswer = question.splice(hiddenElIndex, 1, '..')[0];
+  return [question, correctAnswer];
+};
